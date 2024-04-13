53ce7749812d110c079b48c691fc67f19892dee4
leonls1 <leonlederhossturich@gmail.com>
2024-04-13 18:30:45 -0300

    completed structure basic for CategoryController

--- e-commerce/src/main/java/com/ADN/ecommerce/controller/CategoryController.java
+++ e-commerce/src/main/java/com/ADN/ecommerce/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.ADN.ecommerce.controller;
 
 import com.ADN.ecommerce.model.entities.Category;
+import com.ADN.ecommerce.service.CategoryService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,31 +16,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/category")
 public class CategoryController {
 
-    @GetMapping
-    public ResponseEntity<Category> getById(@PathVariable Long id) {
-        return null;
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getById(@PathVariable("id") Long id) {
+
+        if (!service.existsById(id)) {
+            return new ResponseEntity("that id Doesn't exists: " + id,
+                     HttpStatus.BAD_REQUEST);
+        } else {
+            Category category = service.getById(id).get();
+            return new ResponseEntity(category, HttpStatus.OK);
+        }
     }
 
-    @GetMapping
+    @GetMapping("/getAll")
     public ResponseEntity<List<Category>> getAll() {
-
+        List<Category> list = service.getAll();
+        
+        return new ResponseEntity(list, HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Category> create(@RequestBody Category category) {
-
+        Category cat = service.save(category);
+        return new ResponseEntity(cat, HttpStatus.OK);
+        
     }
 
-    @PutMapping
-    public ResponseEntity<Category> update(@RequestBody Category category, @PathVariable Long id) {
-
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Category> update(@RequestBody Category category, @PathVariable("id") Long id) {
+        if(!service.existsById(id)){
+            return new ResponseEntity("that id Doesn't exists: " + id,
+                     HttpStatus.BAD_REQUEST);
+        } else {
+            Category cat = service.update(category, id);
+            return new ResponseEntity(cat, HttpStatus.OK);
+        }      
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Category> delete(@PathVariable Long id) {
+        if(service.delete(id)){
+            return new ResponseEntity("Deleted Completed", HttpStatus.OK);
+            
+        }else{
+             return new ResponseEntity("that id Doesn't exists: " + id,
+                     HttpStatus.BAD_REQUEST);
+        }
 
     }
 
